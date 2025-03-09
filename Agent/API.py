b8ac700e7481ecd022cca06b3170438d644f7193
Abdelrahman Mostafa Mohamed <11410120200827@stud.cu.edu.eg>
2025-03-09 10:00:17 +0200

    make API in production

--- Agent/API.py
+++ Agent/API.py
@@ -268,5 +268,6 @@ async def query(prompt: str):
 
 
 # Only run Uvicorn when executing this script directly
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8020)
+# if __name__ == "__main__":
+#     # uvicorn.run(app, host="127.0.0.1", port=8020)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
